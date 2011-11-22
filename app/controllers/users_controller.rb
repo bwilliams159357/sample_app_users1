2c8af7e1d91b8c2001c50e9f53fca908516562a3
Brian Williams <bwilliam@cs.utsa.edu>
2011-11-22 10:08:00 -0600

    Added to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,13 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new()
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
+  end
 end
